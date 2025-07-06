a0091260849a65e8459b82765c8bde2b51ea5692
raviattrash-pro <raviattrash1@gmail.com>
2025-07-06 14:30:05 +0530

    stock_app

---
+++ stock_app/stocks_Exchange/stocks_Exchange/src/main/java/com/stocks/stocks_Exchange/StocksExchangeApplication.java
@@ -0,0 +1,15 @@
+package com.stocks.stocks_Exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksExchangeApplication.class, args);
+		System.out.println("Helol World !!!!!!!!!!!!!!!");
+	}
+	
+
+}
---
+++ stock_app/stocks_Exchange/stocks_Exchange/src/test/java/com/stocks/stocks_Exchange/StocksExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocks.stocks_Exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
